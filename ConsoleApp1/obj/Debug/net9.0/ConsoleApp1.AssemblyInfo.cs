48bd5ac6f63ab67139a03d86f385cd18c6b26038
marcoyimyaem <marcoyimyaem@gmail.com>
2025-12-13 23:27:56 +0800

    Day 8

--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71dc1ae6aee3291c02ddb4901656078c50077062")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0675d275e30754cf9bddb6df686fb19a82e1cb92")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
